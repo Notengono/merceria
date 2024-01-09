5b2dbbc5fd81b4cd662d61d014d72f5d313f857c
Martin Remedi <68205330+Notengono@users.noreply.github.com>
2024-01-09 12:30:00 -0300

    siguiendo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: 'https://miturnera.com.ar/merceria/backend/public/'
-  // apiURL: 'http://localhost/merceria/backend/public/index.php/'
+  // apiURL: 'https://miturnera.com.ar/merceria/backend/public/'
+  apiURL: 'http://localhost/merceria/backend/public/index.php/'
 };
