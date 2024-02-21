b616d931041a1a6c985dc8ced54e11c588705b98
Martin Remedi <68205330+Notengono@users.noreply.github.com>
2024-02-21 11:49:37 -0300

    Actualización de búsqueda
    Actualización de búsqueda de productos, ahora permite buscar por el código

--- src/app/components/buscar-producto/buscar-producto.component.ts
+++ src/app/components/buscar-producto/buscar-producto.component.ts
@@ -5,56 +5,59 @@ import { ProductosDetalleI } from 'src/app/model/productos-detalle';
 import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
-  selector: 'app-buscar-producto',
-  templateUrl: './buscar-producto.component.html',
-  styleUrls: ['./buscar-producto.component.css'],
+    selector: 'app-buscar-producto',
+    templateUrl: './buscar-producto.component.html',
+    styleUrls: ['./buscar-producto.component.css'],
 })
 
 export class BuscarProductoComponent implements OnInit {
-  myControl = new FormControl('');
-  option: string[] = ['One', 'Two', 'Three'];
-  filteredoptions: Observable<string[]> | undefined;
+    myControl = new FormControl('');
+    filteredoptions: Observable<string[]> | undefined;
 
-  filtro: string = "";
-  listadoProductos: string[] = []
-  listadoProductosTodos: any[] = []
-  busquedaForm = this.fb.group({
-    inputGroupCategoria: 0,
-    inputGroupSubCategoria: 0
-  })
-
-
-  constructor(private _productos: ProductosService, private fb: FormBuilder) { }
-
-  ngOnInit() {
-    this.listadoProductos = []
-    this._productos.postProductos(this.busquedaForm.value).subscribe(respuesta => {
-      this.listadoProductosTodos = (respuesta['resultado'])
-      console.log(this.listadoProductosTodos)
-      Object.values(respuesta['resultado']).forEach(element => {
-        this.listadoProductos.push((element as ProductosDetalleI).producto)
-      });
-
-      this.filteredoptions = this.myControl.valueChanges.pipe(
-        startWith(''),
-        map(value => this._filter(value || '')),
-      );
+    listadoProductos: string[] = []
+    listadoProductosTodos: any[] = []
+    listadoMostrar: any[] = []
+    busquedaForm = this.fb.group({
+        inputGroupCategoria: 0,
+        inputGroupSubCategoria: 0
     })
-  }
 
-  private _filter(value: string): string[] {
-    if (value.length > 1) {
-      const filterValue = value.toLowerCase();
-      return this.listadoProductos.filter(option => option.toLowerCase().includes(filterValue));
-    } else {
-      return []
-    }
-  }
 
-  onEnter(evt: any) {
-    if (evt.source.selected) {
-      console.log(evt.source.value);
-      this.filtro = evt.source.value;
+    constructor(private _productos: ProductosService, private fb: FormBuilder) { }
+
+    ngOnInit() {
+        this.listadoProductos = []
+        this._productos.postProductos(this.busquedaForm.value).subscribe(respuesta => {
+            this.listadoProductosTodos = (respuesta['resultado'])
+            Object.values(respuesta['resultado']).forEach(element => {
+                this.listadoProductos.push((element as ProductosDetalleI).codigo + ' - ' + (element as ProductosDetalleI).producto)
+            });
+            this.filteredoptions = this.myControl.valueChanges.pipe(
+                startWith(''),
+                map(value => this._filter(value || '')),
+            );
+        })
     }
-  }
+
+    private _filter(value: string): string[] {
+        if (value.length > 1) {
+            const filterValue = value.toLowerCase();
+            return this.listadoProductos.filter(option => option.toLowerCase().includes(filterValue));
+        } else {
+            return []
+        }
+    }
+
+    onEnter(evt: any) {
+        if (evt.source.selected) {
+            const valor = evt.source.value
+            const filterValue = valor.toLowerCase();
+            this.listadoMostrar = this.listadoProductosTodos.filter(option => (option.codigo + ' - ' + option.producto).toLowerCase().includes(filterValue));
+            return true
+        }
+        else {
+            this.listadoMostrar = []
+            return false
+        }
+    }
 }
